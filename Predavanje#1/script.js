7fcd70384537039ec8871d746352ea34616f4284
unknown <alex08dev@gmail.com>
2025-09-27 19:53:57 +0200

    prvo predavanje

---
+++ Predavanje#1/script.js
@@ -0,0 +1,22 @@
+// Vezba
+
+let name = "Aleksandar";
+let surname = "Mijatovic"
+
+let fullName = name + " " + surname;
+
+console.log(fullName);
+
+// Vezba 2
+
+let num1 = 29;
+let num2 = 60;
+
+let sum = num1 + num2;
+console.log(sum);
+
+// Domaci zadatak
+
+let niz = ["Racunar", "tastatura", "mis"];
+
+console.log(niz);
