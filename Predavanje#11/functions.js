d044527fee4b8709bd48161b6f1d8576ad519c25
unknown <alex08dev@gmail.com>
2025-10-16 23:16:40 +0200

    Dodato predavanje 11

---
+++ Predavanje#11/functions.js
@@ -0,0 +1,28 @@
+function sayHello(){
+    console.log("Hello World!");
+}
+
+sayHello();
+
+// Vezba Napraviti funkciju online da ispise poruku sajt je online
+
+function online(){
+    console.log("sajt je online")
+}
+
+online();
+
+// is adult
+
+
+function isAdult(age){
+
+    if (age >= 18){
+        console.log("Adult")
+    } else{
+        console.log("Not Adult")
+    }
+}
+
+isAdult(10);
+isAdult(19);
